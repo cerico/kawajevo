ce65baeeb6e6c6793892fd1bec8648a4b11bbfa1
cerico <garethrobertlee@gmail.com>
2022-06-23 11:42:34 +0200

    yay

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Styles from './styles.module.scss';
+
+function Button({ children }) {
+	return <span className={Styles.button}>{children}</span>;
+}
+
+export default Button;
